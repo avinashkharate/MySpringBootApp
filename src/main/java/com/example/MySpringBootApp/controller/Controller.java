1e4bc34c6b54a7aac2a8938a5f01ebe4c86c3ac1
Avinash Kharate <avinashkharate@Avinashs-MacBook-Air.local>
2025-08-07 11:29:40 +0530

    /hello endpoint added

--- src/main/java/com/example/MySpringBootApp/controller/Controller.java
+++ src/main/java/com/example/MySpringBootApp/controller/Controller.java
@@ -1,4 +1,12 @@
 package com.example.MySpringBootApp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+    @GetMapping("/hello")
+    public String Hello(){
+        return "Hello... , This Jar Was packaged by jenkins Server ";
+    }
 }
