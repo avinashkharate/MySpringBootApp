1581d185fe1f52db77886f9879eae5a8062bb10c
avinashkharate <avinashkharate29@gmail.com>
2025-08-08 23:18:41 +0530

    Product Details added

---
+++ src/main/java/com/example/MySpringBootApp/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.example.MySpringBootApp.controller;
+
+
+import com.example.MySpringBootApp.entity.Product;
+import com.example.MySpringBootApp.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductRepository repo;
+
+    @PostMapping("/product")
+    public Product save(@RequestBody Product product) {
+        return repo.save(product);
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAll() {
+        return repo.findAll();
+    }
+}
